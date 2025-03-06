a0d67f25a0e3030a695fd7366bff5ec8a762cb35
TheSwimon <lordkipanidzegeorge@gmail.com>
2025-03-07 00:14:24 +0400

    initial commit

---
+++ Models/SalesTotal.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DirectoryMCSFT.Models
+{
+    internal class SalesTotal
+    {
+        public double OverallTotal { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using DirectoryMCSFT.Models;
+using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+
+string currentDirectory = Directory.GetCurrentDirectory();
+string storeDirectory = Path.GetFullPath(Path.Combine(currentDirectory, "..", "..", "..","mslearn-dotnet-files","stores"));
+
+// sales of each store
+IEnumerable<string> salesTotals = Directory.EnumerateFiles(storeDirectory, "*salestotals.json", SearchOption.AllDirectories);
+
+// Creating new directory
+string salesTotalDir = Path.Combine(storeDirectory, "SalesTotalDir");
+Directory.CreateDirectory(salesTotalDir);
+
+File.WriteAllText(Path.Combine(salesTotalDir, "totals.txt"), string.Empty);
+double sum = 0;
+
+foreach (var file in salesTotals)
+{
+    var salesJson = File.ReadAllText(file);
+    var salesData = JsonConvert.DeserializeObject<SalesTotal>(salesJson);
+    sum += salesData.OverallTotal;
+}
+
+File.AppendAllText(Path.Combine(salesTotalDir, "totals.txt"), $"total: 200");
